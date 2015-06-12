57998c74a80d668d269baaf62a35896a2486aeca
Jean-Claude Tran <jctran@sutunam.com>
2015-06-12 13:07:43 +0200

    modify ADMIN lines

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -17,10 +17,9 @@
     <link href="{{ URL::asset('assets/admin/layout/css/layout.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ URL::asset('assets/admin/layout/css/themes/darkblue.css') }}" rel="stylesheet" type="text/css" id="style_color"/>
 </head>
-    <body class="page-md page-header-fixed page-quick-sidebar-over-content page-sidebar-closed-hide-logo">
-
-        <div class="page-header md-shadow-z-1-i navbar navbar-fixed-top">
-        <!-- BEGIN HEADER INNER -->
+<body class="page-md page-header-fixed page-quick-sidebar-over-content page-sidebar-closed-hide-logo">
+    <div class="page-header md-shadow-z-1-i navbar navbar-fixed-top">
+    <!-- BEGIN HEADER INNER -->
         <div class="page-header-inner">
             <!-- BEGIN LOGO -->
             <div class="page-logo">
@@ -110,9 +109,52 @@
                         </a>
                         <ul class="sub-menu">
                             <li>
-                                <a href="{{ URL::to('/admin/') }}">
-                                Afficher le pannel d'administration</a>
-                            </li>
+                <a href="javascript:;">
+                <i class="icon-diamond"></i>
+                <span class="title">Catégories</span>
+                <span class="arrow "></span>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="{{ URL::to('admin/categories/') }}">
+                        Gerer les catégories</a>
+                    </li>
+                    <li>
+                        <a href="{{ URL::to('admin/categories/create') }}">
+                        Ajouter une catégorie</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;">
+                <i class="icon-diamond"></i>
+                <span class="title">Jury</span>
+                <span class="arrow "></span>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="{{ URL::to('admin/jurys/') }}">
+                        Gerer le jury</a>
+                    </li>
+                    <li>
+                        <a href="{{ URL::to('admin/jurys/create') }}">
+                        Ajouter un jury</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:;">
+                <i class="icon-diamond"></i>
+                <span class="title">Candidats</span>
+                <span class="arrow "></span>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a href="#">
+                        Gerer les dossiers candidat</a>
+                    </li>
+                </ul>
+            </li>
                         </ul>
                     </li>
                 @endif
@@ -135,47 +177,35 @@
     <footer>
         <p>Bref RH</p>
     </footer>
-
-    <!-- END FOOTER -->
-<!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
-<!-- BEGIN CORE PLUGINS -->
-<!--[if lt IE 9]>
-<script src="../../assets/global/plugins/respond.min.js"></script>
-<script src="../../assets/global/plugins/excanvas.min.js"></script>
-<![endif]-->
-<script src="{{ URL::asset('assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
-<!-- IMPORTANT! Load jquery-ui.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
-<script src="{{ URL::asset('assets/global/plugins/jquery-ui/jquery-ui.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/jquery.cokie.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/uniform/jquery.uniform.min.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="{{ URL::asset('assets/global/scripts/metronic.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/admin/layout/scripts/layout.js') }}" type="text/javascript"></script>
-<script src="{{ URL::asset('assets/admin/layout/scripts/quick-sidebar.js') }}" type="text/javascript"></script>
-<!-- END PAGE LEVEL SCRIPTS -->
-<script>
-jQuery(document).ready(function() {
-   Metronic.init(); // init metronic core componets
-   Layout.init(); // init layout
-   QuickSidebar.init(); // init quick sidebar
-Demo.init(); // init demo features
-   Index.init();
-   Index.initDashboardDaterange();
-   Index.initJQVMAP(); // init index page's custom scripts
-   Index.initCalendar(); // init index page's custom scripts
-   Index.initCharts(); // init index page's custom scripts
-   Index.initChat();
-   Index.initMiniCharts();
-   Tasks.initDashboardWidget();
-});
-</script>
+    
+    <script src="{{ URL::asset('assets/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/jquery-ui/jquery-ui.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/jquery.cokie.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/uniform/jquery.uniform.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/global/scripts/metronic.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/admin/layout/scripts/layout.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('assets/admin/layout/scripts/quick-sidebar.js') }}" type="text/javascript"></script>
+    <script>
+    jQuery(document).ready(function() {
+       Metronic.init(); // init metronic core componets
+       Layout.init(); // init layout
+       QuickSidebar.init(); // init quick sidebar
+    Demo.init(); // init demo features
+       Index.init();
+       Index.initDashboardDaterange();
+       Index.initJQVMAP(); // init index page's custom scripts
+       Index.initCalendar(); // init index page's custom scripts
+       Index.initCharts(); // init index page's custom scripts
+       Index.initChat();
+       Index.initMiniCharts();
+       Tasks.initDashboardWidget();
+    });
+    </script>
 </body>
 </html>
