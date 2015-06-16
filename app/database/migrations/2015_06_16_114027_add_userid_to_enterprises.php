a738c533ca06af523e601734863171725d45ace3
Robin Chalas <rchalas@sutunam.com>
2015-06-16 13:41:52 +0200

    add userid foreign key on enterprises table

---
+++ app/database/migrations/2015_06_16_114027_add_userid_to_enterprises.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUseridToEnterprises extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('enterprises', function(Blueprint $table)
+		{
+			$table->unsignedInteger('user_id')->nullable();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('enterprises', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
