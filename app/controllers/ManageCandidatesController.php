c7ef3a1ba78f7ceb6871aaef80c809704815119d
chalasr <robin.chalas@epitech.eu>
2015-07-21 17:46:58 +0200

    try to fix error on filter candidates

--- app/controllers/ManageCandidatesController.php
+++ app/controllers/ManageCandidatesController.php
@@ -72,7 +72,7 @@ class ManageCandidatesController extends BaseController
         if(!Auth::check()) return Redirect::to('users/register')->with('error', 'Vous devez être inscrit pour accéder à cette         partie du site.');
         if(Auth::user()->role_id != 3) return Redirect::to('users/register')->with('error', 'Vous devez être administrateur pour accéder à cette partie du site');
         $candidate = User::find($id);
-        if($candidate->enterprise()->first())
+        if($candidate)
           $enterprise = $candidate->enterprise()->first();
         $survey = false;
         $activity = false;
